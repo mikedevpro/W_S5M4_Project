4cb7a5c81b16bf1d6510106be41e650ac276b4f9
Michael Nobles <Mnobles33@gmail.com>
2024-08-30 06:11:04 -0700

    My Commit

--- frontend/index.js
+++ frontend/index.js
@@ -4,7 +4,9 @@ async function moduleProject4() {
   const footer = document.querySelector('footer')
   const currentYear = new Date().getFullYear()
   footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
-
+  
+  
+  
   let descriptions = [
     ["Sunny", "☀️"],
     ["Cloudy", "☁️"],
@@ -15,7 +17,60 @@ async function moduleProject4() {
   ]
 
   // 👉 Tasks 1 - 5 go here
+  document.querySelector('#weatherWidget').style.display = 'none'
+  document.querySelector('#citySelect').addEventListener('change', async evt => {
+    console.log('selection changed')
+    try {
+      document.querySelector('#citySelect').setAttribute('disabled', 'disabled')
+      document.querySelector('#weatherWidget').style.display = 'none'
+      document.querySelector('.info').textContent = "Fetching weather data..."
 
+      console.log(evt.target.value)
+      let city = evt.target.value
+      let url = `http://localhost:3003/api/weather?city=${city}`
+      
+      const res = await axios.get(url)
+      document.querySelector('#weatherWidget').style.display = 'block'
+      document.querySelector('.info').textContent = ''
+      evt.target.removeAttribute('disabled')
+
+      let { data } = res
+
+      document.querySelector('#apparentTemp div:nth-child(2)')
+        .textContent = `${data.current.apparent_temperature}°`
+      document.querySelector('#todayDescription')
+        .textContent = descriptions.find(d => d[0] === data.current.weather_description)[1]
+      document.querySelector('#todayStats div:nth-child(1)')
+        .textContent = `${data.current.temperature_min}°/${data.current.temperature_max}°`
+      document.querySelector('#todayStats div:nth-child(2)')
+        .textContent = `Precipitation: ${data.current.precipitation_probability * 100}%`
+      document.querySelector('#todayStats div:nth-child(3)')
+        .textContent = `Humidity: ${data.current.humidity}%`
+      document.querySelector('#todayStats div:nth-child(4)')
+        .textContent = `Wind: ${data.current.wind_speed}m/s`
+
+      data.forcast.daily.forEach((day, idx) => {
+        let card = document.querySelectorAll('.next-day')[idx]
+
+        let weekDay = card.children[0]
+        let apparent = card.children[1]
+        let minMax = card.children[2]
+        let precipit = card.children[3]
+
+        weekDay.textContent = getWeekDay(day.date)
+        apparent.textContent = descriptions.find(d => d[0] === data.current.weather_description)[1]
+        minMax.textContent = `${data.current.temperature_min}°/${data.current.temperature_max}°`
+        precipit.textContent =  `Precipitation: ${data.current.precipitation_probability * 100}%`
+      })
+
+    document.querySelector('#location').firstElementChild.textContent = data.location.city
+    } catch (err) {
+      console.log('Promise rejected with an err.message --> ', err.message)
+    }
+  })
+function getWeekDay(date) {
+  return date
+}
   // 👆 WORK WORK ABOVE THIS LINE 👆
 
 }
